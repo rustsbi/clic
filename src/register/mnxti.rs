d0292c05d94076df86187c6cc6359ee712486428
plucky <m202372036@hust.edu.cn>
2023-10-31 14:53:21 +0800

    register: add mnxti register
    Signed-off-by: plucky <m202372036@hust.edu.cn>

--- src/register/mnxti.rs
+++ src/register/mnxti.rs
@@ -1,4 +1,42 @@
 //! Machine interrupt handler address and enable modifier register.
+use core::arch::asm;
 
-// TODO: this CSR is different than a regular one; it's only designed to use with
-// CSRR(CSRRS rd, csr, x0), CSRRSI and CSRRCI instructions.
+/// Get next interrupt handler address and enable MIE.
+#[inline]
+pub fn next_handler_set_mie() -> usize {
+    let mut val: usize;
+    unsafe { asm!("csrrsi {}, 0x345, 8", out(reg) val) };
+    val
+}
+
+/// Get next interrupt handler address and enable SIE.
+#[inline]
+pub fn next_handler_set_sie() -> usize {
+    let mut val: usize;
+    unsafe { asm!("csrrsi {}, 0x345, 2", out(reg) val) };
+    val
+}
+
+/// Get next interrupt handler address and disable MIE.
+#[inline]
+pub fn next_handler_clear_mie() -> usize {
+    let mut val: usize;
+    unsafe { asm!("csrrci {}, 0x345, 8", out(reg) val) };
+    val
+}
+
+/// Get next interrupt handler address and disable SIE.
+#[inline]
+pub fn next_handler_clear_sie() -> usize {
+    let mut val: usize;
+    unsafe { asm!("csrrci {}, 0x345, 2", out(reg) val) };
+    val
+}
+
+/// Get next interrupt handler address.
+#[inline]
+pub fn next_handler() -> usize {
+    let mut val: usize;
+    unsafe { asm!("csrr {}, 0x345", out(reg) val) };
+    val
+}
